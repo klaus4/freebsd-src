klaus4/freebsd-src

commit d8622b220e4cb7add75776b8c3d6982b3f88ec9c

Use M_NOWAIT in wtab_alloc(), too. Convert panic() to a soft failure here. wtab_alloc() is used by red_alloc(), which can fail.
Reported by:	Kim Culhan <w8hdkim gmail.com>


git-svn-id: svn+ssh://svn.freebsd.org/base/head@240835 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/contrib/altq/altq/altq_red.c
@@ -235,6 +235,13 @@ red_alloc(int weight, int inv_pmax, int th_min, int th_max, int flags,
 	if (rp == NULL)
 		return (NULL);
 
+	/* allocate weight table */
+	rp->red_wtab = wtab_alloc(rp->red_weight);
+	if (rp->red_wtab == NULL) {
+		free(rp, M_DEVBUF);
+		return (NULL);
+	}
+
 	rp->red_avg = 0;
 	rp->red_idle = 1;
 
@@ -300,9 +307,6 @@ red_alloc(int weight, int inv_pmax, int th_min, int th_max, int flags,
 	 */
 	rp->red_probd = (2 * (rp->red_thmax - rp->red_thmin)
 			 * rp->red_inv_pmax) << FP_SHIFT;
-
-	/* allocate weight table */
-	rp->red_wtab = wtab_alloc(rp->red_weight);
 
 	microtime(&rp->red_last);
 	return (rp);
@@ -638,10 +642,9 @@ wtab_alloc(int weight)
 			return (w);
 		}
 
-	w = malloc(sizeof(struct wtab), M_DEVBUF, M_WAITOK);
+	w = malloc(sizeof(struct wtab), M_DEVBUF, M_NOWAIT | M_ZERO);
 	if (w == NULL)
-		panic("wtab_alloc: malloc failed!");
-	bzero(w, sizeof(struct wtab));
+		return (NULL);
 	w->w_weight = weight;
 	w->w_refcount = 1;
 	w->w_next = wtab_list;

commit 13cacaac3a91e443f41a0c54c4895f368be8e009

Fix panic introduced by me in r240835, when zero weight was passed to wtab_alloc().
Reported by:	Kim Culhan <w8hdkim gmail.com>

sys/contrib/altq/altq/altq_red.c
@@ -235,6 +235,11 @@ red_alloc(int weight, int inv_pmax, int th_min, int th_max, int flags,
 	if (rp == NULL)
 		return (NULL);
 
+	if (weight == 0)
+		rp->red_weight = W_WEIGHT;
+	else
+		rp->red_weight = weight;
+
 	/* allocate weight table */
 	rp->red_wtab = wtab_alloc(rp->red_weight);
 	if (rp->red_wtab == NULL) {
@@ -245,10 +250,6 @@ red_alloc(int weight, int inv_pmax, int th_min, int th_max, int flags,
 	rp->red_avg = 0;
 	rp->red_idle = 1;
 
-	if (weight == 0)
-		rp->red_weight = W_WEIGHT;
-	else
-		rp->red_weight = weight;
 	if (inv_pmax == 0)
 		rp->red_inv_pmax = default_inv_pmax;
 	else