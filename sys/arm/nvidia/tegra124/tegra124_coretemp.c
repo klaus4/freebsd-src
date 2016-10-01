klaus4/freebsd-src

commit c1b8c04202a17c815e1c21848160758c245d09bf

TEGRA: Prepare Tegra subtree for inclusion into ARM generic kernel.  - use DEFINE_CLASS_0() for driver classes  - unify driver names  - cleanup driver definitions and bindings
git-svn-id: svn+ssh://svn.freebsd.org/base/head@306477 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/arm/nvidia/tegra124/tegra124_coretemp.c
@@ -250,7 +250,6 @@ tegra124_coretemp_detach(device_t dev)
 	return (0);
 }
 
-
 static device_method_t tegra124_coretemp_methods[] = {
 	/* Device interface */
 	DEVMETHOD(device_identify,	tegra124_coretemp_identify),
@@ -263,11 +262,7 @@ static device_method_t tegra124_coretemp_methods[] = {
 };
 
 static devclass_t tegra124_coretemp_devclass;
-static driver_t tegra124_coretemp_driver = {
-	"tegra124_coretemp",
-	tegra124_coretemp_methods,
-	sizeof(struct tegra124_coretemp_softc),
-};
-
+static DEFINE_CLASS_0(coretemp, tegra124_coretemp_driver,
+    tegra124_coretemp_methods, sizeof(struct tegra124_coretemp_softc));
 DRIVER_MODULE(tegra124_coretemp, cpu, tegra124_coretemp_driver,
-    tegra124_coretemp_devclass, 0, 0);
+    tegra124_coretemp_devclass, NULL, NULL);

sys/arm/nvidia/tegra124/tegra124_cpufreq.c
@@ -588,11 +588,7 @@ static device_method_t tegra124_cpufreq_methods[] = {
 };
 
 static devclass_t tegra124_cpufreq_devclass;
-static driver_t tegra124_cpufreq_driver = {
-	"tegra124_cpufreq",
-	tegra124_cpufreq_methods,
-	sizeof(struct tegra124_cpufreq_softc),
-};
-
+static DEFINE_CLASS_0(cpufreq, tegra124_cpufreq_driver,
+    tegra124_cpufreq_methods, sizeof(struct tegra124_cpufreq_softc));
 DRIVER_MODULE(tegra124_cpufreq, cpu, tegra124_cpufreq_driver,
-    tegra124_cpufreq_devclass, 0, 0);
+    tegra124_cpufreq_devclass, NULL, NULL);

sys/arm/nvidia/as3722.c
@@ -405,7 +405,7 @@ static device_method_t as3722_methods[] = {
 };
 
 static devclass_t as3722_devclass;
-DEFINE_CLASS_0(gpio, as3722_driver, as3722_methods,
+static DEFINE_CLASS_0(gpio, as3722_driver, as3722_methods,
     sizeof(struct as3722_softc));
 EARLY_DRIVER_MODULE(as3722, iicbus, as3722_driver, as3722_devclass,
-    0, 0, 74);
+    NULL, NULL, 74);

sys/arm/nvidia/tegra124/tegra124_car.c
@@ -602,12 +602,7 @@ static device_method_t tegra124_car_methods[] = {
 };
 
 static devclass_t tegra124_car_devclass;
-
-static driver_t tegra124_car_driver = {
-	"tegra124_car",
-	tegra124_car_methods,
-	sizeof(struct tegra124_car_softc),
-};
-
+static DEFINE_CLASS_0(car, tegra124_car_driver, tegra124_car_methods,
+    sizeof(struct tegra124_car_softc));
 EARLY_DRIVER_MODULE(tegra124_car, simplebus, tegra124_car_driver,
-    tegra124_car_devclass, 0, 0, BUS_PASS_TIMER);
+    tegra124_car_devclass, NULL, NULL, BUS_PASS_TIMER);

sys/arm/nvidia/tegra124/tegra124_pmc.c
@@ -555,12 +555,8 @@ static device_method_t tegra124_pmc_methods[] = {
 	DEVMETHOD_END
 };
 
-static driver_t tegra124_pmc_driver = {
-	"tegra124_pmc",
-	tegra124_pmc_methods,
-	sizeof(struct tegra124_pmc_softc),
-};
-
 static devclass_t tegra124_pmc_devclass;
+static DEFINE_CLASS_0(pmc, tegra124_pmc_driver, tegra124_pmc_methods,
+    sizeof(struct tegra124_pmc_softc));
 EARLY_DRIVER_MODULE(tegra124_pmc, simplebus, tegra124_pmc_driver,
-    tegra124_pmc_devclass, 0, 0, 70);
+    tegra124_pmc_devclass, NULL, NULL, 70);

sys/arm/nvidia/tegra124/tegra124_xusbpadctl.c
@@ -575,7 +575,6 @@ xusbpadctl_attach(device_t dev)
 	return (0);
 }
 
-
 static device_method_t tegra_xusbpadctl_methods[] = {
 	/* Device interface */
 	DEVMETHOD(device_probe,         xusbpadctl_probe),
@@ -591,13 +590,8 @@ static device_method_t tegra_xusbpadctl_methods[] = {
 	DEVMETHOD_END
 };
 
-static driver_t tegra_xusbpadctl_driver = {
-	"tegra_xusbpadctl",
-	tegra_xusbpadctl_methods,
-	sizeof(struct xusbpadctl_softc),
-};
-
 static devclass_t tegra_xusbpadctl_devclass;
-
+static DEFINE_CLASS_0(xusbpadctl, tegra_xusbpadctl_driver,
+    tegra_xusbpadctl_methods, sizeof(struct xusbpadctl_softc));
 EARLY_DRIVER_MODULE(tegra_xusbpadctl, simplebus, tegra_xusbpadctl_driver,
-    tegra_xusbpadctl_devclass, 0, 0, 73);
+    tegra_xusbpadctl_devclass, NULL, NULL, 73);

sys/arm/nvidia/tegra_abpmisc.c
@@ -187,8 +187,8 @@ static device_method_t tegra_abpmisc_methods[] = {
 	DEVMETHOD_END
 };
 
-DEFINE_CLASS_0(tegra_abpmisc, tegra_abpmisc_driver, tegra_abpmisc_methods,
-    sizeof(struct tegra_abpmisc_softc));
 static devclass_t tegra_abpmisc_devclass;
+static DEFINE_CLASS_0(abpmisc, tegra_abpmisc_driver, tegra_abpmisc_methods,
+    sizeof(struct tegra_abpmisc_softc));
 EARLY_DRIVER_MODULE(tegra_abpmisc, simplebus, tegra_abpmisc_driver,
-    tegra_abpmisc_devclass, 0, 0, BUS_PASS_TIMER);
+    tegra_abpmisc_devclass, NULL, NULL, BUS_PASS_TIMER);

sys/arm/nvidia/tegra_ahci.c
@@ -602,7 +602,6 @@ tegra_ahci_resume(device_t dev)
 	return (bus_generic_resume(dev));
 }
 
-static devclass_t tegra_ahci_devclass;
 static device_method_t tegra_ahci_methods[] = {
 	DEVMETHOD(device_probe,		tegra_ahci_probe),
 	DEVMETHOD(device_attach,	tegra_ahci_attach),
@@ -620,9 +619,8 @@ static device_method_t tegra_ahci_methods[] = {
 	DEVMETHOD_END
 };
 
-static driver_t tegra_ahci_driver = {
-	"ahci",
-	tegra_ahci_methods,
-	sizeof(struct tegra_ahci_sc)
-};
-DRIVER_MODULE(tegra_ahci, simplebus, tegra_ahci_driver, tegra_ahci_devclass, NULL, NULL);
+static devclass_t tegra_ahci_devclass;
+static DEFINE_CLASS_0(ahci, tegra_ahci_driver, tegra_ahci_methods,
+    sizeof(struct tegra_ahci_sc));
+DRIVER_MODULE(tegra_ahci, simplebus, tegra_ahci_driver, tegra_ahci_devclass,
+    NULL, NULL);

commit 157530bb72dfcf3bc577d7a6a4db7e019587243b

TEGRA: Fix bindings for cpufreq and coretemp drivers, it was broken in r306477. Correct a description for coretemp driver.

sys/arm/nvidia/tegra124/tegra124_coretemp.c
@@ -179,9 +179,9 @@ static void
 tegra124_coretemp_identify(driver_t *driver, device_t parent)
 {
 
-	if (device_find_child(parent, "tegra124_coretemp", -1) != NULL)
+	if (device_find_child(parent, "coretemp", -1) != NULL)
 		return;
-	if (BUS_ADD_CHILD(parent, 0, "tegra124_coretemp", -1) == NULL)
+	if (BUS_ADD_CHILD(parent, 0, "coretemp", -1) == NULL)
 		device_printf(parent, "add child failed\n");
 }
 
@@ -189,7 +189,7 @@ static int
 tegra124_coretemp_probe(device_t dev)
 {
 
-	device_set_desc(dev, "CPU Frequency Control");
+	device_set_desc(dev, "CPU Thermal Sensor");
 	return (0);
 }
 

sys/arm/nvidia/tegra124/tegra124_cpufreq.c
@@ -476,6 +476,8 @@ static void
 tegra124_cpufreq_identify(driver_t *driver, device_t parent)
 {
 
+	if (device_get_unit(parent) != 0)
+		return;
 	if (device_find_child(parent, "tegra124_cpufreq", -1) != NULL)
 		return;
 	if (BUS_ADD_CHILD(parent, 0, "tegra124_cpufreq", -1) == NULL)
@@ -486,8 +488,6 @@ static int
 tegra124_cpufreq_probe(device_t dev)
 {
 
-	if (device_get_unit(dev) != 0)
-		return (ENXIO);
 	device_set_desc(dev, "CPU Frequency Control");
 
 	return (0);
@@ -588,7 +588,7 @@ static device_method_t tegra124_cpufreq_methods[] = {
 };
 
 static devclass_t tegra124_cpufreq_devclass;
-static DEFINE_CLASS_0(cpufreq, tegra124_cpufreq_driver,
+static DEFINE_CLASS_0(tegra124_cpufreq, tegra124_cpufreq_driver,
     tegra124_cpufreq_methods, sizeof(struct tegra124_cpufreq_softc));
 DRIVER_MODULE(tegra124_cpufreq, cpu, tegra124_cpufreq_driver,
     tegra124_cpufreq_devclass, NULL, NULL);