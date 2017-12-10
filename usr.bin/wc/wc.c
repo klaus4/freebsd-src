klaus4/freebsd-src

commit 7fa261f018344d615937d7d04fb5916d605182dc

wc(1): Extend non-controversial optimizations to '-c' mode
wc(1)'s slow path for counting words or multibyte characters requires
conversion of the 8-bit input stream to wide characters.  However, a faster
path can be used for counting only lines ('-l' -- newlines have the same
representation in all supported encodings) or bytes ('-c').

The existing line count optimization was not used if the input was the
implicit stdin.  Additionally, it wasn't used if only byte counting was
requested.  This change expands the fast path to both of these scenarios.

Expanding the buffer size from 64 kB helps reduce the number of read(2)
calls needed, but exactly what impact that change has and what size to
expand the buffer to are still under discussion.

PR:		224160
Tested by:	wosch (earlier version)
Sponsored by:	Dell EMC Isilon


git-svn-id: svn+ssh://svn.freebsd.org/base/head@326736 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

usr.bin/wc/wc.c
@@ -206,30 +206,30 @@ cnt(const char *file)
 	linect = wordct = charct = llct = tmpll = 0;
 	if (file == NULL)
 		fd = STDIN_FILENO;
-	else {
-		if ((fd = open(file, O_RDONLY, 0)) < 0) {
-			xo_warn("%s: open", file);
-			return (1);
-		}
-		if (doword || (domulti && MB_CUR_MAX != 1))
-			goto word;
-		/*
-		 * Line counting is split out because it's a lot faster to get
-		 * lines than to get words, since the word count requires some
-		 * logic.
-		 */
-		if (doline) {
-			while ((len = read(fd, buf, MAXBSIZE))) {
-				if (len == -1) {
-					xo_warn("%s: read", file);
-					(void)close(fd);
-					return (1);
-				}
-				if (siginfo) {
-					show_cnt(file, linect, wordct, charct,
-					    llct);
-				}
-				charct += len;
+	else if ((fd = open(file, O_RDONLY, 0)) < 0) {
+		xo_warn("%s: open", file);
+		return (1);
+	}
+	if (doword || (domulti && MB_CUR_MAX != 1))
+		goto word;
+	/*
+	 * Line counting is split out because it's a lot faster to get
+	 * lines than to get words, since the word count requires some
+	 * logic.
+	 */
+	if (doline || dochar) {
+		while ((len = read(fd, buf, MAXBSIZE))) {
+			if (len == -1) {
+				xo_warn("%s: read", file);
+				(void)close(fd);
+				return (1);
+			}
+			if (siginfo) {
+				show_cnt(file, linect, wordct, charct,
+				    llct);
+			}
+			charct += len;
+			if (doline) {
 				for (p = buf; len--; ++p)
 					if (*p == '\n') {
 						if (tmpll > llct)
@@ -239,36 +239,37 @@ cnt(const char *file)
 					} else
 						tmpll++;
 			}
-			reset_siginfo();
+		}
+		reset_siginfo();
+		if (doline)
 			tlinect += linect;
-			if (dochar)
-				tcharct += charct;
-			if (dolongline) {
-				if (llct > tlongline)
-					tlongline = llct;
-			}
+		if (dochar)
+			tcharct += charct;
+		if (dolongline) {
+			if (llct > tlongline)
+				tlongline = llct;
+		}
+		show_cnt(file, linect, wordct, charct, llct);
+		(void)close(fd);
+		return (0);
+	}
+	/*
+	 * If all we need is the number of characters and it's a
+	 * regular file, just stat the puppy.
+	 */
+	if (dochar || domulti) {
+		if (fstat(fd, &sb)) {
+			xo_warn("%s: fstat", file);
+			(void)close(fd);
+			return (1);
+		}
+		if (S_ISREG(sb.st_mode)) {
+			reset_siginfo();
+			charct = sb.st_size;
 			show_cnt(file, linect, wordct, charct, llct);
+			tcharct += charct;
 			(void)close(fd);
 			return (0);
-		}
-		/*
-		 * If all we need is the number of characters and it's a
-		 * regular file, just stat the puppy.
-		 */
-		if (dochar || domulti) {
-			if (fstat(fd, &sb)) {
-				xo_warn("%s: fstat", file);
-				(void)close(fd);
-				return (1);
-			}
-			if (S_ISREG(sb.st_mode)) {
-				reset_siginfo();
-				charct = sb.st_size;
-				show_cnt(file, linect, wordct, charct, llct);
-				tcharct += charct;
-				(void)close(fd);
-				return (0);
-			}
 		}
 	}
 

commit 049286a591fdef7c2532e81684df46fe88eccad2

wc(1): Restore regular file char count fast path
fstat(2) is going to be a lot faster than reading all of the bytes in a
file, if we just need a character count for a regular file.  This fast path
was accidentally broken in r326736.

PR:		224160
Reported by:	bde
Sponsored by:	Dell EMC Isilon

usr.bin/wc/wc.c
@@ -213,51 +213,10 @@ cnt(const char *file)
 	if (doword || (domulti && MB_CUR_MAX != 1))
 		goto word;
 	/*
-	 * Line counting is split out because it's a lot faster to get
-	 * lines than to get words, since the word count requires some
-	 * logic.
+	 * If all we need is the number of characters and it's a regular file,
+	 * just stat it.
 	 */
-	if (doline || dochar) {
-		while ((len = read(fd, buf, MAXBSIZE))) {
-			if (len == -1) {
-				xo_warn("%s: read", file);
-				(void)close(fd);
-				return (1);
-			}
-			if (siginfo) {
-				show_cnt(file, linect, wordct, charct,
-				    llct);
-			}
-			charct += len;
-			if (doline) {
-				for (p = buf; len--; ++p)
-					if (*p == '\n') {
-						if (tmpll > llct)
-							llct = tmpll;
-						tmpll = 0;
-						++linect;
-					} else
-						tmpll++;
-			}
-		}
-		reset_siginfo();
-		if (doline)
-			tlinect += linect;
-		if (dochar)
-			tcharct += charct;
-		if (dolongline) {
-			if (llct > tlongline)
-				tlongline = llct;
-		}
-		show_cnt(file, linect, wordct, charct, llct);
-		(void)close(fd);
-		return (0);
-	}
-	/*
-	 * If all we need is the number of characters and it's a
-	 * regular file, just stat the puppy.
-	 */
-	if (dochar || domulti) {
+	if (doline == 0) {
 		if (fstat(fd, &sb)) {
 			xo_warn("%s: fstat", file);
 			(void)close(fd);
@@ -272,6 +231,44 @@ cnt(const char *file)
 			return (0);
 		}
 	}
+	/*
+	 * For files we can't stat, or if we need line counting, slurp the
+	 * file.  Line counting is split out because it's a lot faster to get
+	 * lines than to get words, since the word count requires locale
+	 * handling.
+	 */
+	while ((len = read(fd, buf, MAXBSIZE))) {
+		if (len == -1) {
+			xo_warn("%s: read", file);
+			(void)close(fd);
+			return (1);
+		}
+		if (siginfo)
+			show_cnt(file, linect, wordct, charct, llct);
+		charct += len;
+		if (doline) {
+			for (p = buf; len--; ++p)
+				if (*p == '\n') {
+					if (tmpll > llct)
+						llct = tmpll;
+					tmpll = 0;
+					++linect;
+				} else
+					tmpll++;
+		}
+	}
+	reset_siginfo();
+	if (doline)
+		tlinect += linect;
+	if (dochar)
+		tcharct += charct;
+	if (dolongline) {
+		if (llct > tlongline)
+			tlongline = llct;
+	}
+	show_cnt(file, linect, wordct, charct, llct);
+	(void)close(fd);
+	return (0);
 
 	/* Do it the hard way... */
 word:	gotsp = 1;